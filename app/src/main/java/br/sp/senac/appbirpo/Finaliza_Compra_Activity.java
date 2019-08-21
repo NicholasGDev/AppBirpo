e28523a8c3cdf93fe38101273e33680bf36b5dc1
NicholasGDev <nicholas.goulart1@gmail.com>
2019-08-21 15:57:44 -0300

    21/08/2018

---
+++ app/src/main/java/br/sp/senac/appbirpo/Finaliza_Compra_Activity.java
@@ -0,0 +1,14 @@
+package br.sp.senac.appbirpo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Finaliza_Compra_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.finaliza__compra_layout);
+    }
+}
---
+++ app/src/main/java/br/sp/senac/appbirpo/Produtos_Activity.java
@@ -0,0 +1,14 @@
+package br.sp.senac.appbirpo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Produtos_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.produtos_layout);
+    }
+}
